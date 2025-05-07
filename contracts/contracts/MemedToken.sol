079ce1ab19ee528a1df0e06121618efe9dfa7444
Furkan Nabi Sumji <0xfurqaan@gmail.com>
2025-05-07 12:58:32 +0530

    Remove Token contract and deployment script; add MemedToken, MemedFactory, and MemedAirdrop contracts with deployment script

---
+++ contracts/contracts/MemedToken.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./MemedFactory.sol";
+
+contract MemedToken is ERC20, Ownable {
+    MemedFactory factory;
+    uint256 public constant MAX_SUPPLY = 1_000_000_000 * 1e18;
+
+    constructor(
+        string memory _name,
+        string memory _ticker,
+        address _factory,
+        address _to
+    ) ERC20(_name, _ticker) Ownable() {
+        factory = MemedFactory(_factory);
+        _mint(_to, MAX_SUPPLY);
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        super._transfer(from, to, amount);
+        if (balanceOf(from) >= 100 * 10 ** decimals()) {
+            factory.follow(from);
+        } else {
+            factory.unfollow(from);
+        }
+        if (balanceOf(to) >= 100 * 10 ** decimals()) {
+            factory.follow(to);
+        } else {
+            factory.unfollow(to);
+        }
+    }
+}
--- contracts/deploy/deploy.ts
+++ contracts/deploy/deploy.ts
@@ -4,7 +4,13 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 export default async function (hre: HardhatRuntimeEnvironment) {
   const wallet = getWallet();
 
-  await deployContract("Token", [], {
+  /*await deployContract("MemedAirdrop", ["0xdcb603bc2745b1ff3a7e8c437d7bb1ddce77a170874b3e233103d2750a937f00"], {
+    hre,
+    wallet,
+    verify: true,
+  });*/
+
+  await deployContract("MemedFactory", ["0xF077fd1bAC70e6D58b1aF77284FBFC5B75Ce168B"], {
     hre,
     wallet,
     verify: true,
