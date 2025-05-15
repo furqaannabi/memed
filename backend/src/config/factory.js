87700c2f4552ea110abbd3661e0d42c7d44ed6a4
Furkan Nabi Sumji <0xfurqaan@gmail.com>
2025-05-15 17:20:04 +0530

    log removed

--- backend/src/config/factory.js
+++ backend/src/config/factory.js
@@ -2,7 +2,6 @@ const ethers = require('ethers');
 const createMemeABI = require('./abi.json');
 const airdropABI = require('./airdropABI.json');
 const config = require('./config.json');
-console.log(config);
 const provider = new ethers.JsonRpcProvider(process.env.RPC_URL_LENS);
 const wallet = new ethers.Wallet(process.env.ADMIN_PVT_KEY, provider);
 const factory_contract = new ethers.Contract(config.factory, createMemeABI, wallet);  
