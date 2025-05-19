4e83293c52161ab0959b6d7463c1186b84bf10b8
Furkan Nabi Sumji <0xfurqaan@gmail.com>
2025-05-19 15:19:30 +0530

    fixes

--- backend/src/controllers/webhookController.js
+++ backend/src/controllers/webhookController.js
@@ -10,11 +10,24 @@ const Reward = require('../models/Reward');
  * Processes Ethereum logs from alerts
  */
 const webhook = async (req, res) => {
+    console.log(JSON.stringify(req.body));
     res.json({ success: true });
     try {
         if (req.body.type === 'Reward') {
             const {token, userAmount, index} = req.body.data;
-            const tokenData = await Token.findOne({ tokenAddress: token });
+            let tokenData = await Token.findOne({ tokenAddress: token });
+            if (!tokenData) {
+                await new Promise((resolve, reject) => {
+                    setTimeout(async () => {
+                        tokenData = await Token.findOne({ tokenAddress: token });
+                        resolve(tokenData);
+                    }, 4000);
+                });
+            }
+            if (!tokenData) {
+                console.log('Token not found');
+                return;
+            }
             const tokenAirdrops = await Airdrop.find({ token: tokenData._id });
             const distributed = new Airdrop({
                 token: tokenData._id,
@@ -28,12 +41,12 @@ const webhook = async (req, res) => {
             await distributed.save();
         }
         if (req.body.type === 'Claimed') {
-            const {userAddress, amount, index} = req.body.data;
+            const {userAddress, amount, index, transactionHash} = req.body.data;
 
-            const rewards = await Reward.find({ userAddress, amount: ethers.formatUnits(amount, 18), claimed: false }).populate('airdrop');
-            const reward = (rewards.filter(reward => reward.airdrop.index === parseInt(index.toString())))[0];
+            const rewards = await Reward.find({ userAddress, amount, claimed: false }).populate('airdrop');
+            const reward = (rewards.filter(reward => reward.airdrop.index == index))[0];
             if (reward) {
-                await Reward.findByIdAndUpdate(reward._id, { claimed: true });
+                await Reward.findByIdAndUpdate(reward._id, { claimed: true, transactionHash });
             }
         }
     } catch (error) {
